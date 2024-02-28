48525ab3761e7fb1017b715ec1d9b5f748003dd5
faysal725 <afaysal725@gmail.com>
2024-02-28 09:47:56 +0600

    finished the app

--- src/components/Table/ProductTable.jsx
+++ src/components/Table/ProductTable.jsx
@@ -51,6 +51,7 @@ export default function ProductTable({
       <div className=" flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            {/* product table  */}
             <table className="min-w-full divide-y divide-gray-300">
               <tbody className="">
                 {tableData.length > 0 ? (
